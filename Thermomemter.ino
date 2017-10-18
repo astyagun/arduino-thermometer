7bda1d26a3eb64f673b6b39365ec0feb9718bcf1
Anton Styagun <astyagun@gmail.com>
2017-10-18 13:51:05 +0300

    Display temperature on LCD

--- Thermomemter.ino
+++ Thermomemter.ino
@@ -1,14 +1,16 @@
 // DS18B20+ sensor library: https://github.com/milesburton/Arduino-Temperature-Control-Library
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include <LiquidCrystal.h>
 
-#define DEBUG
+/* #define DEBUG */
 
 // Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 2
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
 
 void setup()
 {
@@ -18,6 +20,8 @@ void setup()
   #endif
 
   sensors.begin();
+  lcd.begin(16, 2);
+  lcd.print("Temperature:");
 }
 
 void loop()
@@ -28,6 +32,10 @@ void loop()
 
   sensors.requestTemperatures();
   float temperature = sensors.getTempCByIndex(0);
+
+  lcd.setCursor(0, 1);
+  lcd.print(temperature, 2);
+  lcd.print(" deg. C");
 
   #ifdef DEBUG
     Serial.println("DONE");
