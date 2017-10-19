e9b50d5c2b8a7eb3d184091b0439fce030803de5
Anton Styagun <astyagun@gmail.com>
2017-10-19 16:26:57 +0300

    Remove TODO comments

--- Thermomemter.ino
+++ Thermomemter.ino
@@ -1,7 +1,3 @@
-// TODO: Read in background to prevent blocking reaction to button press
-// TODO: Cache thermometer value to read it less often
-// TODO: Turn display on by button and turn it off after delay
-
 #define REFRESH_DELAY 5000
 
 #include "temperature_sensor.h"
